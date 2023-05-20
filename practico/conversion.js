ccb8162564186ccaff9da90e9770d6e2acaf96a9
Emaponce94 <emanuelponce18@gmail.com>
2023-05-20 12:05:54 -0300

    actualize folder

--- practico/conversion.js
+++ practico/conversion.js
@@ -1,3 +1,3 @@
 let grados_fagrenheit = 50
-let grados_celcius = (gadros_fagrenheit - 32)* (05/9)
-console.log(grados_celcius)
+let grados_celsius = (grados_fagrenheit - 32)* (05/9)
+console.log(grados_celsius)
