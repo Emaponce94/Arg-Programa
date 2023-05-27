c4da8e05ea4ce13679fbcb8e292e1cc8cc33210e
Emaponce94 <emanuelponce18@gmail.com>
2023-05-27 12:46:28 -0300

    Practico N0 finalizado

--- practico0/areaRectangulo.js
+++ practico0/areaRectangulo.js
@@ -1,4 +1,12 @@
-let base = 5 
-let altura = 3
-let areaRecatangulo = (base*altura)
-console.log(areaRecatangulo)
+//Consigna A
+let base = 5 ;
+let altura = 3;
+let areaRectangulo = (base*altura);
+console.log(areaRectangulo);
+
+
+//Consigna B
+let baseB = 12.5;
+let alturaB = 5.3;
+let areaRectanguloB = (baseB*alturaB);
+console.log(areaRectanguloB);
--- practico0/promedio3.js
+++ practico0/promedio3.js
@@ -1,5 +1,20 @@
+//A
 let num1 = 5;
 let num2 = 7;
 let num3 = 9;
 let promedio = (num1+num2+num3) /3;
 console.log(promedio)
+
+//B
+let num1B = -2;
+let num2B = -3;
+let num3B = -8;
+let promedioB = (num1B+num2B+num3B) /3;
+console.log(promedioB)
+
+//C
+let num1C = -1.2;
+let num2C = 5.3;
+let num3C = -3.3;
+let promedioC = (num1C+num2C+num3C) /3;
+console.log(promedioC)
