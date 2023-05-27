1ea12fbc54874c16e3aa9984b3e0f798bdf64acf
Emaponce94 <emanuelponce18@gmail.com>
2023-05-27 12:44:50 -0300

    agrego el archivo conversionGrados.js

--- practico0/conversionGrados.js
+++ practico0/conversionGrados.js
@@ -1,3 +1,9 @@
+//A
 let grados_fagrenheit = 50
 let grados_celsius = (grados_fagrenheit - 32)* (05/9)
 console.log(grados_celsius)
+
+//B
+let grados_fagrenheitB = 75
+let grados_celsiusB = (grados_fagrenheitB - 32)* (05/9)
+console.log(grados_celsiusB)
