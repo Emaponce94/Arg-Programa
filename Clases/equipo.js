e85381be62b99bb61880d8898365dcc3429023d8
Emaponce94 <114536305+Emaponce94@users.noreply.github.com>
2023-05-30 19:19:51 -0300

    Delete Clases directory

--- Clases/equipo.js
+++
@@ -1,7 +0,0 @@
-let jugador1 = {nombre: "Juan", posicion: "arquero", edad: 22};
-let jugador2 = {nombre: "Pedro", posicion: "defensor", edad: 25};
-let jugador3 = {nombre: "Pablo", posicion: "medio", edad: 21};
-let jugador4 = {nombre: "Mateo", posicion: "medio", edad: 20};
-let jugador5 = {nombre: "Lucas", posicion: "delantero", edad: 26};
-let equipo = [jugador1,jugador2,jugador3,jugador4,jugador5];
-console.log('Posicion de '+equipo[1].nombre+ ' es '+equipo[1].posicion);
